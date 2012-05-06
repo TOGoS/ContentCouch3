e1efee707a5cc5c828a8f127e484c6dd830c89e3
TOGoS <togos00@gmail.com>
2012-05-05 20:27:16 -0500

    Rearranged code in FlowUploader a lot.

--- java-src/togos/ccouch3/FlowUploader.java
+++ java-src/togos/ccouch3/FlowUploader.java
@@ -8,16 +8,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import togos.ccouch3.FlowUploader.StandardTransferTracker.Counter;
 import togos.ccouch3.cmdstream.CmdReader;
 import togos.ccouch3.cmdstream.CmdWriter;
 import togos.ccouch3.hash.BitprintDigest;
 
 public class FlowUploader
 {
+	//// Utility classes ////
+	
 	interface MessageDigestFactory {
 		public MessageDigest createMessageDigest();
 	}
@@ -55,39 +62,19 @@ public class FlowUploader
 		}
 	}
 	
-	static class UploadTask {
-		public final String name;
-		public final String path;
-		
-		public UploadTask( String name, String path ) {
-			this.name = name;
-			this.path = path;
-		}
-		
-		@Override
-		public String toString() { return "UploadTask name="+name+", path="+path; }
+	interface Sink<E> {
+		public void give( E value ) throws Exception;
 	}
 	
-	/** Marks the end of a stream of messages, usually indicating that the recipient can quit. */
-	static class EndMessage {
-		public static final EndMessage INSTANCE = new EndMessage();
-		private EndMessage() {}
-		@Override
-		public String toString() { return "EndMessage"; }
+	static class QueueSink<E> implements Sink<E> {
+		protected final BlockingQueue<E> q;
+		public QueueSink( BlockingQueue<E> q ) { this.q = q; }
+		public void give(E value) throws Exception { q.put(value); }
 	}
 	
-	////
-	
-	Collection<UploadTask> tasks;
-	
-	public FlowUploader( Collection<UploadTask> tasks ) {
-		this.tasks = tasks;
-	}
-	
-	static abstract class QueueRunner extends Thread {
+	static abstract class QueueRunner implements Runnable {
 		BlockingQueue<Object> inQueue;
-		public QueueRunner( BlockingQueue<Object> inQueue, String name ) {
-			super(name);
+		public QueueRunner( BlockingQueue<Object> inQueue ) {
 			this.inQueue = inQueue;
 		}
 		protected abstract boolean handleMessage( Object m ) throws Exception;
@@ -109,6 +96,56 @@ public class FlowUploader
 				}
 			}
 		}
+	}
+	
+	interface TransferTracker {
+		// Units = e.g. files, or whatever you want to track.
+		public void transferred( long byteCount, int unitCount, String tag );
+	}
+	
+	static class StandardTransferTracker implements TransferTracker {
+		class Counter {
+			long byteCount = 0;
+			long unitCount = 0;
+		}
+		
+		HashMap<String,Counter> counters = new HashMap<String,Counter>();
+		protected Counter getCounter( String name ) {
+			Counter c = counters.get(name);
+			if( c == null ) counters.put( name, c = new Counter() );
+			return c;
+		}
+		
+		@Override
+		public synchronized void transferred(long byteCount, int unitCount, String tag) {
+			Counter c = getCounter(tag);
+			c.byteCount += byteCount;
+			c.unitCount += unitCount;
+			
+		}
+	}
+	
+	//// Message types ////
+	
+	static class UploadTask {
+		public final String name;
+		public final String path;
+		
+		public UploadTask( String name, String path ) {
+			this.name = name;
+			this.path = path;
+		}
+		
+		@Override
+		public String toString() { return "UploadTask name="+name+", path="+path; }
+	}
+	
+	/** Marks the end of a stream of messages, usually indicating that the recipient can quit. */
+	static class EndMessage {
+		public static final EndMessage INSTANCE = new EndMessage();
+		private EndMessage() {}
+		@Override
+		public String toString() { return "EndMessage"; }
 	}
 	
 	static class BlobInfo {
@@ -141,109 +178,12 @@ public class FlowUploader
 		}
 	}
 	
+	////
+	
 	static final String[] IGNORE_FILENAMES = {
 		"thumbs.db", "desktop.ini"
 	};
-	
-	interface Sink<E> {
-		public void give( E value ) throws Exception;
-	}
-	
-	static class QueueSink<E> implements Sink<E> {
-		protected final BlockingQueue<E> q;
-		public QueueSink( BlockingQueue<E> q ) { this.q = q; }
-		public void give(E value) throws Exception { q.put(value); }
-	}
-	
-	class HeadRequestSender implements Sink<Object> {
-		protected final CmdWriter w;
-		protected final Sink<Object> shortCut;
 		
-		boolean sentAnything = false;
-		
-		public HeadRequestSender( CmdWriter w, Sink<Object> shortCut ) {
-			this.w = w;
-			this.shortCut = shortCut;
-		}
-		
-		protected void bye() throws Exception {
-			if( !sentAnything ) shortCut.give(EndMessage.INSTANCE);
-			w.bye();
-		}
-		
-		@Override
-		public void give( Object m ) throws Exception {
-			if( m instanceof FileInfo ) {
-				FileInfo fi = (FileInfo)m;
-				w.writeCmd( new String[]{ "head", fi.path, fi.urn } );
-			} else if( m instanceof BlobInfo ) {
-				BlobInfo bi = (BlobInfo)m;
-				w.writeCmd( new String[]{ "put", bi.urn, bi.urn, "chunk", String.valueOf(bi.blob.length) } );
-				w.writeChunk( bi.blob, 0, bi.blob.length );
-				w.endChunks();
-			} else if( m instanceof FullyStoredMarker ) {
-				FullyStoredMarker fsm = (FullyStoredMarker)m;
-				w.writeCmd( new String[]{ "echo", "fully-stored", fsm.urn} );
-			} else if( m instanceof EndMessage ) {
-				bye();
-			} else {
-				bye();
-				throw new RuntimeException("Don't know what to do with "+m.getClass());
-			}
-			sentAnything = true;
-		}
-	}
-	
-	class CmdResponseReader implements Runnable {
-		protected final CmdReader r;
-		protected final Sink<Object> messageSink;
-		
-		public CmdResponseReader( CmdReader r, Sink<Object> messageSink ) {
-			this.r = r;
-			this.messageSink = messageSink;
-		}
-		
-		public void run() {
-			String[] m;
-			try {
-				while( (m = r.readCmd()) != null ) {
-					String responseType = m[0];
-					if( "error".equals(responseType) ) {
-						throw new RuntimeException("Error from server: "+CmdWriter.encode(m));
-					} else if( "ok".equals(responseType) ) {
-						if( "head".equals(m[1]) ) {
-							// 0:ok 1:head 2:<path> 3:<urn> 4:{found|missing}
-							boolean found = "found".equals(m[4]);
-							if( found ) {
-								messageSink.give( new FullyStoredMarker(m[3]) );
-							} else {
-								messageSink.give( new FileMissing(m[2], m[3]) );
-							}
-						} else if( "put".equals(m[1]) && m.length >= 3 ) {
-							// 0:ok 1:put 2:<urn> 3:<urn>
-							// Then one of our blobs went through; woot.
-							messageSink.give( new FullyStoredMarker(m[3]) );
-						} else if( "echo".equals(m[1]) && m.length == 4 && "fully-stored".equals(m[2]) ) {
-							// 0:ok 1:echo 2:fully-stored 3:<urn>
-							messageSink.give( new FullyStoredMarker(m[2]) );
-						} else if( "bye".equals(m[1]) ) {
-							// Goodbye!
-						} else {
-							throw new RuntimeException("Unexpected result line from server: "+CmdWriter.encode(m));
-						}
-					} else {
-						throw new RuntimeException("Unexpected message from server: "+CmdWriter.encode(m));
-					}
-				}
-			} catch( Exception e ) {
-				throw new RuntimeException(e);
-			} finally {
-				try { r.close(); } catch( IOException e ) {}
-				try { messageSink.give( EndMessage.INSTANCE ); } catch( Exception e ) {}
-			}
-		}
-	}
-	
 	static class Indexer
 	{
 		protected final DirectorySerializer dirSer;
@@ -326,9 +266,107 @@ public class FlowUploader
 		}
 	}
 	
+	class HeadRequestSender implements Sink<Object> {
+		protected final CmdWriter w;
+		protected final Sink<Object> shortCut;
+		protected final TransferTracker tt;
+		
+		boolean sentAnything = false;
+		
+		public HeadRequestSender( CmdWriter w, Sink<Object> shortCut, TransferTracker tt ) {
+			this.w = w;
+			this.shortCut = shortCut;
+			this.tt = tt;
+		}
+		
+		protected void bye() throws Exception {
+			if( !sentAnything ) shortCut.give(EndMessage.INSTANCE);
+			w.bye();
+		}
+		
+		@Override
+		public void give( Object m ) throws Exception {
+			if( m instanceof FileInfo ) {
+				FileInfo fi = (FileInfo)m;
+				w.writeCmd( new String[]{ "head", fi.path, fi.urn } );
+			} else if( m instanceof BlobInfo ) {
+				BlobInfo bi = (BlobInfo)m;
+				w.writeCmd( new String[]{ "put", bi.urn, bi.urn, "chunk", String.valueOf(bi.blob.length) } );
+				w.writeChunk( bi.blob, 0, bi.blob.length );
+				w.endChunks();
+				tt.transferred( bi.blob.length, 1, "treenode" );
+			} else if( m instanceof FullyStoredMarker ) {
+				FullyStoredMarker fsm = (FullyStoredMarker)m;
+				w.writeCmd( new String[]{ "echo", "fully-stored", fsm.urn} );
+			} else if( m instanceof EndMessage ) {
+				bye();
+			} else {
+				bye();
+				throw new RuntimeException("Don't know what to do with "+m.getClass());
+			}
+			sentAnything = true;
+		}
+	}
+	
+	class CmdResponseReader implements Runnable {
+		protected final CmdReader r;
+		protected final Sink<Object> messageSink;
+		
+		public CmdResponseReader( CmdReader r, Sink<Object> messageSink ) {
+			this.r = r;
+			this.messageSink = messageSink;
+		}
+		
+		public void run() {
+			String[] m;
+			try {
+				while( (m = r.readCmd()) != null ) {
+					String responseType = m[0];
+					if( "error".equals(responseType) ) {
+						throw new RuntimeException("Error from server: "+CmdWriter.encode(m));
+					} else if( "ok".equals(responseType) ) {
+						if( "head".equals(m[1]) ) {
+							// 0:ok 1:head 2:<path> 3:<urn> 4:{found|missing}
+							boolean found = "found".equals(m[4]);
+							if( found ) {
+								messageSink.give( new FullyStoredMarker(m[3]) );
+							} else {
+								messageSink.give( new FileMissing(m[2], m[3]) );
+							}
+						} else if( "put".equals(m[1]) && m.length >= 3 ) {
+							// 0:ok 1:put 2:<urn> 3:<urn>
+							// Then one of our blobs went through; woot.
+							messageSink.give( new FullyStoredMarker(m[3]) );
+						} else if( "echo".equals(m[1]) && m.length == 4 && "fully-stored".equals(m[2]) ) {
+							// 0:ok 1:echo 2:fully-stored 3:<urn>
+							messageSink.give( new FullyStoredMarker(m[2]) );
+						} else if( "bye".equals(m[1]) ) {
+							// Goodbye!
+						} else {
+							throw new RuntimeException("Unexpected result line from server: "+CmdWriter.encode(m));
+						}
+					} else {
+						throw new RuntimeException("Unexpected message from server: "+CmdWriter.encode(m));
+					}
+				}
+			} catch( Exception e ) {
+				throw new RuntimeException(e);
+			} finally {
+				try { r.close(); } catch( IOException e ) {}
+				try { messageSink.give( EndMessage.INSTANCE ); } catch( Exception e ) {}
+			}
+		}
+	}
+	
 	class Uploader implements Sink<Object> {
 		protected final CmdWriter w;
-		public Uploader( CmdWriter w ) { this.w = w; }
+		protected final TransferTracker tt;
+		public Uploader( CmdWriter w, TransferTracker tt ) {
+			this.w = w;
+			this.tt = tt;
+		}
+		
+		public int uploadCount = 0;
 		
 		@Override
 		public void give(Object m) throws Exception {
@@ -340,8 +378,10 @@ public class FlowUploader
 				FileInputStream fis = new FileInputStream(f);
 				for( int z = fis.read(buffer); z >= 0; z = fis.read(buffer) ) {
 					w.writeChunk( buffer, 0, z );
+					tt.transferred( z, 0, "file" );
 				}
 				w.endChunks();
+				tt.transferred( 0, 1, "file" );
 			} else if( m instanceof FullyStoredMarker ) {
 				// TODO: Mark the stuff as stored!
 			} else if( m instanceof EndMessage ) {
@@ -350,6 +390,15 @@ public class FlowUploader
 				throw new RuntimeException("Unexpected message: "+m.toString());
 			}
 		}
+	}
+	
+	//// Put it all together ////
+	
+	Collection<UploadTask> tasks;
+	public boolean showTransferSummary;
+		
+	public FlowUploader( Collection<UploadTask> tasks ) {
+		this.tasks = tasks;
 	}
 	
 	public void run() {
@@ -361,8 +410,9 @@ public class FlowUploader
 		} catch( IOException e ) {
 			throw new RuntimeException("Couldn't run cmdserver via exec");
 		}
-		final Uploader uploader = new Uploader( new CmdWriter(uploadProc.getOutputStream()) );
-		final HeadRequestSender headRequestor = new HeadRequestSender( new CmdWriter(headProc.getOutputStream()), uploader );
+		final StandardTransferTracker tt = new StandardTransferTracker();
+		final Uploader uploader = new Uploader( new CmdWriter(uploadProc.getOutputStream()), tt );
+		final HeadRequestSender headRequestor = new HeadRequestSender( new CmdWriter(headProc.getOutputStream()), uploader, tt );
 		final CmdResponseReader headResponseReader = new CmdResponseReader(
 			new CmdReader(headProc.getInputStream()),
 			uploader
@@ -392,7 +442,6 @@ public class FlowUploader
 				return "urn:bitprint:" + BitprintDigest.format(hash);
 			}
 		});
-		
 		final DirectorySerializer dirSer = new NewStyleRDFDirectorySerializer();
 		final Indexer indexer = new Indexer( dirSer, digestor, new Sink<Object>() {
 			@Override
@@ -400,8 +449,7 @@ public class FlowUploader
 				headRequestor.give(m);
 			}
 		});
-		
-		final Thread indexThread = new QueueRunner( uploadTaskQueue, "Indexer" ) {			
+		final QueueRunner indexRunner = new QueueRunner( uploadTaskQueue ) {
 			public boolean handleMessage( Object m ) throws Exception {
 				if( m instanceof UploadTask ) {
 					UploadTask ut = (UploadTask)m;
@@ -421,9 +469,8 @@ public class FlowUploader
 				logMessageQueue.add( EndMessage.INSTANCE );
 			}
 		};
-		final Thread headResponseReaderThread = new Thread( headResponseReader, "Head Response Reader" );
-		final Thread uploadResponseReaderThread = new Thread( uploadResponseReader, "Upload Response Reader" );
-		final Thread logThread   = new QueueRunner( logMessageQueue, "Log Writer" ) {
+
+		final QueueRunner logRunner   = new QueueRunner( logMessageQueue ) {
 			@Override
 			protected boolean handleMessage(Object m) throws Exception {
 				if( m instanceof DirectoryEntry ) {
@@ -440,6 +487,11 @@ public class FlowUploader
 			@Override
 			protected void cleanUp() throws Exception {}
 		};
+		
+		final Thread indexThread = new Thread( indexRunner, "Indexer" );
+		final Thread headResponseReaderThread = new Thread( headResponseReader, "Head Response Reader" );
+		final Thread uploadResponseReaderThread = new Thread( uploadResponseReader, "Upload Response Reader" );
+		final Thread logThread = new Thread( logRunner, "Log Writer" );
 		
 		indexThread.start();
 		headResponseReaderThread.start();
@@ -460,14 +512,37 @@ public class FlowUploader
 			logThread.interrupt();
 			Thread.currentThread().interrupt();
 		}
+		
+		if( showTransferSummary ) {
+			if( tt.counters.isEmpty() ) {
+				System.err.println( "No transfers!" );
+			}
+			for( Map.Entry<String,Counter> thing : tt.counters.entrySet() ) {
+				System.err.println( "Transferred "+thing.getValue().byteCount+" bytes in "+thing.getValue().unitCount+" "+thing.getKey()+"s");
+			}
+		}
+	}
+	
+	public static int main( Iterator<String> args ) throws Exception {
+		ArrayList<UploadTask> tasks = new ArrayList<UploadTask>();
+		boolean verbose = false; // false!;
+		for( ; args.hasNext(); ) {
+			String a = args.next();
+			if( "-v".equals(a) ) {
+				verbose = true;
+			} else if( !a.startsWith("-") ) {
+				tasks.add( new UploadTask(a, a) );
+			} else {
+				return 1;
+			}
+		}
+		FlowUploader fu = new FlowUploader(tasks);
+		fu.showTransferSummary = verbose;
+		fu.run();
+		return 0;
 	}
 	
 	public static void main( String[] args ) throws Exception {
-		ArrayList<UploadTask> tasks = new ArrayList<UploadTask>();
-		for( String a : args ) {
-			tasks.add( new UploadTask(a, a) );
-		}
-		FlowUploader fu = new FlowUploader(tasks);
-		fu.run();
+		System.exit(main( Arrays.asList(args).iterator() ));
 	}
 }
