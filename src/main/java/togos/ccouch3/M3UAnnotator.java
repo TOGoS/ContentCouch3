e8b13250869cf139ea00ffe2a1a63b808e441b2c
TOGoS <togos00@gmail.com>
2015-10-13 16:58:18 -0500

    Improvements to annotate-m3u.
    - Will trust #URN: lines if encountered.
    - Otherwise, not finding a file is not a fatal error
      (unprocessable paths will be passed through).
    - Add -strict option to make that a fatal error.
    - Improved help text.

--- src/main/java/togos/ccouch3/M3UAnnotator.java
+++ src/main/java/togos/ccouch3/M3UAnnotator.java
@@ -24,22 +24,32 @@ public class M3UAnnotator
 		"  -?                     ; print extended help text to stdout and exit\n"+
 		"  -transform <transform> ; indicate path transformation (default 'none')\n"+
 		"  -tidy-filenames        ; tidy up filenames (spaces to underscores, etc)\n"+
-		"                         ; when generating filename hints from paths\n";
+		"                         ; when generating filename hints from paths\n"+
+		"  -strict                ; Exit(1) if any entries cannot be annotated.\n";
 	protected static final String USAGE =
 		SHORT_USAGE+
 		"\n"+
 		"Path transformation specifications:\n"+
 		"  none - paths are left as they are\n"+
 		"  raw:<server> - URLs of the form <server>/uri-res/raw/<urn>/<filename>\n"+
+		"  N2R:<server> - URLs of the form <server>/uri-res/N2R?<urn>\n"+
 		"\n"+
 		"Default mode of operation is to emit a #URN:<urn> line for each\n"+
-		"file referenced from the M3U.\n";
+		"file referenced from the M3U.\n"+
+		"\n"+
+		"If the input file contains #URN: lines, those will be used as the URN\n"+
+		"for the following file.  Otherwise the file will be read and its hash\n"+
+		"calculated.  If the file cannot be found, the entry is passed through\n"+
+		"unmodified and a warning printed to stderr.  If -strict is passed, this\n"+
+		"situation will also result in a non-zero exit code.\n";
 	
 	enum PathTransform {
 		NONE,
 		RAW,
 		N2R
 	}
+	
+	protected static final String URN_LINE_PREFIX = "#URN:";
 	
 	protected static String basename( String path ) {
 		int idx = Math.max(path.lastIndexOf("/"), path.lastIndexOf("\\"));
@@ -50,12 +60,16 @@ public class M3UAnnotator
 		PathTransform pathTransform = PathTransform.NONE;
 		String defaultN2rPrefix = null;
 		boolean tidyFilenames = false;
+		int missingFileCount = 0;
+		boolean strict = false;
 		
 		ArrayList<String> m3uPaths = new ArrayList<String>();
 		while( argi.hasNext() ) {
 			String arg = argi.next();
 			if( "-".equals(arg) || !arg.startsWith("-") ) {
 				m3uPaths.add(arg);
+			} else if( "-strict".equals(arg) ) {
+				strict = true;
 			} else if( "-tidy-filenames".equals(arg) ) {
 				tidyFilenames = true;
 			} else if( "-transform".equals(arg) ) {
@@ -136,6 +150,7 @@ public class M3UAnnotator
 		
 		try {
 			for( String p : m3uPaths ) {
+				String providedUrn = null;
 				ByteBlob b = argumentResolver.getBlob(p);
 				BufferedReader br = new BufferedReader(new InputStreamReader(b.openInputStream()));
 				try {
@@ -143,11 +158,25 @@ public class M3UAnnotator
 					while( (line = br.readLine()) != null ) {
 						String t = line.trim();
 						if( t.length() > 0 && !t.startsWith("#") ) {
-							String urn = identifier.identifyResource(t);
+							String urn = providedUrn;
+							if( urn == null ) {
+								try {
+									urn = identifier.identifyResource(t);
+								} catch( FileNotFoundException e ) {
+									System.err.println("Couldn't find file to identify it: "+t);
+									++missingFileCount;
+								}
+							}
+							String path;
 							switch( pathTransform ) {
 							case NONE:
+								path = line;
 								break;
 							case RAW:
+								if( urn == null ) {
+									path = line;
+									break;
+								}
 								String name = basename(t);
 								if( tidyFilenames ) {
 									name = name.replace(" (", "-");
@@ -157,18 +186,30 @@ public class M3UAnnotator
 									name = name.replace("&", "+");
 									name = name.replaceAll("[^a-zA-Z0-9_+.-]", "");
 								}
-								line = defaultN2rPrefix+urn+"/"+URLEncoder.encode(name, "UTF-8").replace("+", "%20");
+								path = defaultN2rPrefix+urn+"/"+URLEncoder.encode(name, "UTF-8").replace("+", "%20");
 								break;
 							case N2R:
-								line = defaultN2rPrefix+urn;
+								if( urn == null ) {
+									path = line;
+									break;
+								}
+								path = defaultN2rPrefix+urn;
 								break;
 							default:
 								throw new RuntimeException("Unrecognized path transform enum value: "+pathTransform);
 							}
 							
-							System.out.println("#URN:"+urn);
+							if( urn != null ) System.out.println(URN_LINE_PREFIX+urn);
+							System.out.println(path);
+							providedUrn = null;
+						} else {
+							if( line.startsWith(URN_LINE_PREFIX) ) {
+								providedUrn = line.substring(URN_LINE_PREFIX.length()).trim();
+							} else {
+								// Everything else, just copy it through.
+								System.out.println(line);
+							}
 						}
-						System.out.println(line);
 					}
 				} finally {
 					br.close();
@@ -177,6 +218,12 @@ public class M3UAnnotator
 		} catch( IOException e ) {
 			e.printStackTrace(System.err);
 			return 1;
+		}
+		if( missingFileCount > 0 ) {
+			System.err.println(missingFileCount+" file(s) couln't be found.");
+			System.err.println("No annotations were added and no path transformations");
+			System.err.println("were done for them.");
+			if( strict ) return 1;
 		}
 		return 0;
 	}
