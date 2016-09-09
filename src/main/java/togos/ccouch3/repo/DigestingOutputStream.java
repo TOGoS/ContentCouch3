7a5fa0d64cd45a3bf516a59a1a8e29c01558c994
TOGoS <togos00@gmail.com>
2016-09-09 16:01:56 -0500

    DigestingOutputStream.

---
+++ src/main/java/togos/ccouch3/repo/DigestingOutputStream.java
@@ -0,0 +1,41 @@
+package togos.ccouch3.repo;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.security.MessageDigest;
+
+public class DigestingOutputStream extends OutputStream
+{
+	protected final MessageDigest digest;
+	protected final OutputStream backingOutputStream;
+	
+	public DigestingOutputStream( MessageDigest digest, OutputStream backingOutputStream ) {
+		this.digest = digest;
+		this.backingOutputStream = backingOutputStream;
+	}
+	
+	@Override public void write(int b) throws IOException {
+		digest.update((byte)b);
+		backingOutputStream.write(b);
+	}
+	
+	@Override public void write(byte[] b, int off, int len) throws IOException {
+		digest.update(b, off, len);
+		backingOutputStream.write(b, off, len);
+	}
+	
+	@Override public void write(byte[] b) throws IOException {
+		write(b, 0, b.length);
+	}
+	
+	public MessageDigest getMessageDigest() {
+		return digest;
+	}
+	public byte[] digest() {
+		return digest.digest();
+	}
+	
+	@Override public void close() throws IOException {
+		backingOutputStream.close();
+	}
+}
