644f5ed97f7ba828d8a67925464598693dbc4e21
TOGoS <togos00@gmail.com>
2023-12-28 20:08:17 -0600

    Work on this WalkFilesystemCmd thing

--- src/main/java/togos/ccouch3/WalkFilesystemCmd.java
+++ src/main/java/togos/ccouch3/WalkFilesystemCmd.java
@@ -8,11 +8,15 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.ProcessBuilder.Redirect;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import togos.ccouch3.hash.BitprintDigest;
 import togos.ccouch3.util.Charsets;
+import togos.ccouch3.util.DateUtil;
 
 /**
  * Simple tool for walking the filesystem and collecting
@@ -40,7 +44,19 @@ public class WalkFilesystemCmd {
 		return sb.toString();
 	}
 	
+	boolean includeDirs = true;
+	boolean includeFiles = true;
+	boolean recurse = true;
+	boolean beChatty = true;
+	float extraErrorChance = 0.1f;
+	Random rand = new Random(new Date().getTime());
+	Pattern namePattern = Pattern.compile("^[^.].*");
+	
 	protected String getFileKey(File f) throws IOException, InterruptedException {
+		if( extraErrorChance > 0 && rand.nextFloat() < extraErrorChance ) {
+			throw new IOException("Synthetic IOException when calculating file key");
+		}
+		
 		ProcessBuilder pb = new ProcessBuilder();
 		String[] cmd = new String[] { "fsutil","file","queryfileid",f.getPath() };
 		pb.command(cmd);
@@ -62,6 +78,10 @@ public class WalkFilesystemCmd {
 	}
 	
 	public String getFileBitprint(File f) throws IOException {
+		if( extraErrorChance > 0 && rand.nextFloat() < extraErrorChance ) {
+			throw new IOException("Synthetic IOException when calculating bitprint");
+		}
+		
 		BitprintDigest digest = new BitprintDigest();
 		try( FileInputStream fis = new FileInputStream(f) ) {
 			int z;
@@ -73,27 +93,46 @@ public class WalkFilesystemCmd {
 		}
 	}
 	
-	protected void leaf(File f) {
+	class FileInfo {
+		public final String path;
+		public final long size;
+		public final long mtime;
+		public final String fileKey;
+		public final String bitprint;
+		public FileInfo(String path, long size, long mtime, String fileKey, String bitprint) {
+			this.path = path;
+			this.size = size;
+			this.mtime = mtime;
+			this.fileKey = fileKey;
+			this.bitprint = bitprint;
+		}
 	}
 	
-	boolean includeDirs = true;
-	boolean includeFiles = true;
-	boolean recurse = true;
-	Pattern namePattern = Pattern.compile("^[^.].*");
+	protected void leaf(FileInfo fi) {
+		System.out.println(fi.path+"\t"+fi.size+"\t"+DateUtil.formatDate(fi.mtime)+"\t"+fi.fileKey+"\t"+fi.bitprint);
+	}
 	
-	public void walk(File f, String asName) throws InterruptedException {
+	protected int walk(File f, String asName) throws InterruptedException {
 		boolean isDir = f.isDirectory();
 		boolean include = isDir ? includeDirs : includeFiles;
+		int errorCount = 0;
 		
 		if( include ) {
 			String bitprint;
 			String fileKey;
+			
+			long size = f.length();
+			// Note: 0 might be returned from f.length() in some cases that I
+			// might want to distinguish from actually-a-zero-length-file.
+			
+			long mtime = f.lastModified();
 			
 			try {
 				bitprint = isDir ? null : getFileBitprint(f);
 			} catch( IOException e ) {
 				System.err.print("Failed to get bitprint for "+f+": ");
 				bitprint = "ERROR";
+				++errorCount;
 			}
 			
 			try {
@@ -102,18 +141,44 @@ public class WalkFilesystemCmd {
 				System.err.print("Failed to get fileKey for "+f+": ");
 				e.printStackTrace();
 				fileKey = "ERROR";
+				++errorCount;
 			}
 			
-			System.out.println(asName+"\t"+fileKey+"\t"+bitprint);
+			leaf(new FileInfo(asName, size, mtime, fileKey, bitprint));
 		}
 		
 		if( f.isDirectory() && recurse ) {
 			File[] files = f.listFiles();
-			for( File fil : files ) {
+			if( files == null ) {
+				++errorCount;
+				System.out.println("# Failed to read directory entries from "+asName);
+				System.err.println("Failed to read entries from "+f.getPath());
+			} else for( File fil : files ) {
 				if( namePattern.matcher(fil.getName()).matches() ) {
-					walk(fil, asName+"/"+fil.getName());
+					errorCount += walk(fil, asName+"/"+fil.getName());
 				}
 			}
+		}
+		
+		return errorCount;
+	}
+	
+	public void walk(List<Pair<String,File>> roots) throws InterruptedException {
+		Date startDate = new Date();
+		if( beChatty ) {
+			System.out.println("# "+getClass().getSimpleName()+"#walk starting at "+DateUtil.formatDate(startDate));
+		}
+		System.err.println("#COLUMNS:path\tsize\tmtime\tfilekey\tbitprinturn");
+		
+		int errorCount = 0;
+		for( Pair<String,File> root : roots ) {
+			errorCount += walk(root.right, root.left);
+		}
+		Date endDate = new Date();
+		if( beChatty ) {
+			System.out.println("# "+getClass().getSimpleName()+"#walk finishing at "+DateUtil.formatDate(startDate));
+			System.out.println("# Processing took "+(endDate.getTime()-startDate.getTime())/1000+" seconds");
+			System.out.println("# There were "+errorCount+" errors");
 		}
 	}
 	
@@ -151,8 +216,6 @@ public class WalkFilesystemCmd {
 		
 		WalkFilesystemCmd cmd = new WalkFilesystemCmd();
 		
-		for( Pair<String,File> root : roots ) {
-			cmd.walk(root.right, root.left);
-		}
+		cmd.walk(roots);
 	}
 }
