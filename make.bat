f8449894748f134071bc27ddfc0813d825b7651b
TOGoS <togos00@gmail.com>
2024-02-24 16:10:31 -0600

    Write a make.bat that actually kinda works

--- make.bat
+++ make.bat
@@ -1,1 +1,25 @@
-make.exe "find_exe=C:\Program Files\Git\usr\bin\find.exe" "pathsep=;" 
+@echo off
+
+rem If make worked, I wouldn't have a separate make.bat!
+rem make.exe "find_exe=C:\Program Files\Git\usr\bin\find.exe" "pathsep=;" 
+
+setlocal
+
+set any_errors=
+
+if not defined UNIX_FIND_EXE echo Plase define UNIX_FIND_EXE>&2 && set any_errors=1
+
+if not defined JAVAC_CMD set "JAVAC_CMD=javac"
+if not defined JAR_CMD set "JAR_CMD=jar"
+
+if defined any_errors goto fail
+
+"%UNIX_FIND_EXE%" src/main ext-lib/*/src/main -name '*.java' >.java-main-src.lst
+rmdir /s/q target\main
+mkdir target/main
+%JAVAC_CMD% -source 1.7 -target 1.7 -sourcepath src/main/java -d target/main @.java-main-src.lst
+rm CCouch3.jar
+%JAR_CMD% -ce togos.ccouch3.CCouch3Command -C target/main . >CCouch3.jar
+
+:fail
+exit /B 1
